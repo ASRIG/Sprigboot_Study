1d71d6a16497f4dd8c67825cea12e1e2170accb1
asrig9 <cmychs@gmail.com>
2020-05-24 02:15:02 +0900

    init

---
+++ src/main/java/com/spring/hstb01/HongYoutubeStb01Application.java
@@ -0,0 +1,13 @@
+package com.spring.hstb01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongYoutubeStb01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongYoutubeStb01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/hstb01/HongYoutubeStb01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.hstb01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongYoutubeStb01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
